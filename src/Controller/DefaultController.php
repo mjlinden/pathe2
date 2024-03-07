78e44010502f47ec54122ecaa185db20604f4871
mjlinden <m.j.vd.linden@rocmondriaan.nl>
2024-03-07 12:53:17 +0100

    form commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Department;
+use App\Form\DepartmentType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,7 +23,7 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    #[Route('/department/{id}', name: 'app_department')]
+    #[Route('/department/{id<\d+>}', name: 'app_department')]
     public function index2(EntityManagerInterface $entityManager,$id): Response
     {
         $department=$entityManager->getRepository(Department::class)->find($id);
@@ -29,6 +31,28 @@ class DefaultController extends AbstractController
         return $this->render('default/employees.html.twig', [
             'department' => $department,
 
+        ]);
+    }
+
+    #[Route('/new', name: 'app_new_department')]
+    public function new(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $department=new Department();
+        $form=$this->createForm(DepartmentType::class, $department);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $department = $form->getData();
+            //dd($department);
+            $entityManager->persist($department);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_default');
+        }
+
+        return $this->render('default/new.html.twig', [
+            'form' => $form,
         ]);
     }
 
